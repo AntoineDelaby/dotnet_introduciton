5f299e7c027c70b6154126e97962ef9041992d32
Antoine Delaby <103427041+AntoineDelaby@users.noreply.github.com>
2023-09-19 16:44:39 +0200

    First commit

---
+++ src/Leonardo/Fibonacci.cs
@@ -0,0 +1,46 @@
+﻿namespace Leonardo;
+
+public class Fibonacci
+{
+    private static int Run(int i)
+    {
+        if (i <= 2)
+        {
+            return 1;
+        }
+
+        return Run(i - 1) + Run(i - 2);
+    }
+    
+    public static async Task<IList<int>> RunAsync(string[] args)
+    {
+
+        if (args.Length >= 100)
+        {
+            throw new ArgumentException("Too much");
+        }
+        
+        IList<int> results = new List<int>();
+
+        var tasks = new List<Task<int>>();
+        foreach (var arg in args)
+        {
+            var task = Task.Run(() =>
+            {
+                var result = Fibonacci.Run(int.Parse(arg));
+                return result;
+            });
+            tasks.Add(task);
+        }
+
+        foreach (var task in tasks)
+        {
+            var result = await task;
+            //var result = task.Result; -> // Même chose que la ligne du dessus
+            Console.WriteLine($"Result : {result}");
+            results.Add(task.Result);
+        }
+
+        return results;
+    }
+}
---
+++ tests/Leonardo.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Leonardo.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var results = await Fibonacci.RunAsync(new string[] {"1", "2", "3"});
+        Assert.Equal(1, results[0]);
+        Assert.Equal(1, results[1]);
+        Assert.Equal(2, results[2]);
+    }
+}
